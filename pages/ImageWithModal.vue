8288d51c08c10dace2e3a6ef9189abcd58d7ce2e
Kevin López Brante <kevin@kddlb.cl>
2021-02-23 23:06:46 -0300

    Added screenshots to features page

---
+++ pages/ImageWithModal.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="pb-3">
+    <a href="" @click.prevent="$bvModal.show(imgKey)">
+      <b-img :src="source" fluid thumbnail/>
+    </a>
+    <b-modal :id="imgKey" size="xl" :title="title">
+      <template slot="modal-footer">
+        <b-form-checkbox id="light-mode-chooser"
+                         v-model="mode"
+                         unchecked-value="light"
+                         value="dark"
+                         switch>{{mode.charAt(0).toUpperCase() + mode.slice(1)}} mode</b-form-checkbox>
+      </template>
+      <b-img :src="source" fluid thumbnail/>
+    </b-modal>
+  </div>
+</template>
+<script>
+export default {
+  name: 'image-with-modal',
+  props: ['imgKey', 'mode', 'title'],
+  computed: {
+    source() {
+      return `https://balde.losno.co/cog/screenshots/${this.imgKey}@${this.mode}.png`
+    }
+  }
+}
+</script>
--- pages/features.vue
+++ pages/features.vue
@@ -1,15 +1,42 @@
 <template>
 <article>
-  <h1>{{page.title}}</h1>
-  <nuxt-content :document="page" />
+  <b-row>
+    <b-col>
+      <h1>{{page.title}}</h1>
+      <nuxt-content :document="page" />
+    </b-col>
+    <b-col>
+      <aside>
+        <h1>Screenshots</h1>
+        <b-form-checkbox id="light-mode-chooser"
+                         v-model="imageMode"
+                         unchecked-value="light"
+                         value="dark"
+                         switch>{{imageMode.charAt(0).toUpperCase() + imageMode.slice(1)}} mode</b-form-checkbox>
+        <image-with-modal title="Cog full window" imgKey="cog" :mode.sync="imageMode"/>
+        <image-with-modal title="Cog mini window" imgKey="mini" :mode.sync="imageMode"/>
+        <image-with-modal title="Cog mini window with volume control" imgKey="mini-vol" :mode.sync="imageMode"/>
+      </aside>
+    </b-col>
+  </b-row>
+
+
 </article>
 </template>
 
 <script>
+import ImageWithModal from "~/pages/ImageWithModal";
+
 export default {
   name: "features",
+  components: {ImageWithModal},
   head: {
     title: "Cog features",
+  },
+  data() {
+    return {
+      imageMode: "light"
+    }
   },
   async asyncData({$content}) {
     return {
@@ -19,6 +46,3 @@ export default {
 }
 </script>
 
-<style scoped>
-
-</style>
